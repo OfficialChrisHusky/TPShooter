640b07fbd2bfa657280bd2d5d5389a38e315cb79
OfficialChrisHusky <chrishuskywolf@gmail.com>
2023-06-07 19:15:35 +0200

    Wall Running

--- TPShooter/Assets/Scripts/Player/PlayerController.cs
+++ TPShooter/Assets/Scripts/Player/PlayerController.cs
@@ -23,6 +23,14 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float jumpForce = 10.0f;
     [SerializeField] private float airDrag = 5.0f;
 
+    [Header("Wall Running")]
+    [SerializeField] private bool onWall;
+    [SerializeField] private float wallDistance = 0.6f;
+    [SerializeField] private float minJumpHeight = 1.5f;
+    [SerializeField] private float wallRunGravity = 1.0f;
+    [SerializeField] private float wallRunMultiplier = 1.5f;
+    [SerializeField] private float cancelJumpForce;
+
     [Header("Ground Detection")]
     [SerializeField] private bool grounded;
     [SerializeField] private LayerMask groundMask;
@@ -30,10 +38,13 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float groundDistance = 0.4f;
 
     private Rigidbody rb;
+    private Vector3 force;
 
     private Vector3 direction;
 
     private Vector2 mouseRotation;
+
+    private RaycastHit wallHit;
 
     void Start() {
         
@@ -45,6 +56,8 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
+
+        CheckWall();
         
         mouseRotation.y += Input.GetAxis("Mouse X") * sensitivity;
         mouseRotation.x -= Input.GetAxis("Mouse Y") * sensitivity;
@@ -68,7 +81,7 @@ public class PlayerController : MonoBehaviour {
         } else if(!grounded && rb.drag != airDrag) {
 
             rb.drag = airDrag;
-            multiplier = 0.3f;
+            multiplier = 1.0f;
 
         }
 
@@ -84,8 +97,50 @@ public class PlayerController : MonoBehaviour {
 
     }
     void FixedUpdate() {
-        
+
+        if(onWall) {
+
+            multiplier = wallRunMultiplier;
+            rb.useGravity = false;
+            rb.AddForce(Vector3.down * wallRunGravity);
+
+            if(Input.GetKeyDown(KeyCode.Space)) {
+
+                Vector3 cancelDir = transform.up + wallHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+                rb.AddForce(cancelDir * cancelJumpForce, ForceMode.Impulse);
+
+            }
+
+        } else {
+
+            rb.useGravity = true;
+            if(grounded && Input.GetKey(KeyCode.LeftShift))
+                multiplier = sprintMultiplier;
+            else if(grounded && !Input.GetKey(KeyCode.LeftShift))
+                multiplier = 1.0f;
+            else
+                multiplier = 1.0f;
+
+        }
+
         rb.AddForce(direction.normalized * speed * multiplier * Time.deltaTime * 100.0f, ForceMode.Acceleration);
+
+    }
+
+    void CheckWall() {
+
+        if(grounded) {
+            
+            onWall = false;
+            return;
+            
+        }
+
+        onWall = Physics.Raycast(transform.position, -transform.right, out wallHit, wallDistance);
+        
+        if (onWall) return;
+        onWall = Physics.Raycast(transform.position,  transform.right, out wallHit, wallDistance);
 
     }
 
